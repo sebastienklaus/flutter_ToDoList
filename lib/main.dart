188b97f4df18453966724052ede4bae04e5d1fa6
sebastienklaus <sebastienklaus7@gmail.com>
2022-01-19 17:57:59 +0100

    change class name + add fonts + splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -14,58 +14,12 @@ class TodoList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Todo List',
       theme: ThemeData(
-        primarySwatch: Colors.pink,
+        primarySwatch: Colors.blue,
+        fontFamily: 'ShadowsIntoLight',
       ),
-      home: const MyHomePage(title: 'ToDo List'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key); //constructeur
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Hello:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: const AllTasks(title: 'ToDo List'),
     );
   }
 }
--- lib/screens/all_tasks.dart
+++ lib/screens/all_tasks.dart
@@ -1,15 +1,49 @@
 import 'package:flutter/material.dart';
 
 class AllTasks extends StatefulWidget {
-  const AllTasks({Key? key}) : super(key: key);
+  const AllTasks({Key? key, required this.title}) : super(key: key);//constrcuteur
+
+  final String title;
 
   @override
   _AllTasksState createState() => _AllTasksState();
 }
 
 class _AllTasksState extends State<AllTasks> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Hello:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
+
