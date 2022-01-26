4357f88fb4f77d9af3c4b611e4a74c2fef1943ed
sebastienklaus <sebastienklaus7@gmail.com>
2022-01-26 23:17:30 +0100

    try to change the parameter in the constrcutor

--- lib/components/tasks/task_master.dart
+++ lib/components/tasks/task_master.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:todo_list/data/tasks.dart';
+import 'package:todo_list/models/task.dart';
 
 class TaskMaster extends StatelessWidget {
-  const TaskMaster({Key? key, required dataTasks})
-      : super(key: key); //constrcuteur
-
+  const TaskMaster({Key? key, required this.dataTasks}) : super(key: key);
+  //constrcuteur
+  final List<Task> dataTasks;
   @override
   Widget build(BuildContext context) {
     return Center(
       child: ListView.builder(
-        itemCount: tasks.length,
+        itemCount: dataTasks.length,
         itemBuilder: (context, index) {
           return ListTile(
             leading: const Icon(Icons.check_box_outline_blank_rounded),
-            title: Text(tasks[index].content),
-            subtitle: Text('Created at ${tasks[index].createdAt}'),
+            title: Text(dataTasks[index].content),
+            subtitle: Text('Created at ${dataTasks[index].createdAt}'),
             trailing: const Icon(Icons.drag_handle_rounded),
           );
         },
