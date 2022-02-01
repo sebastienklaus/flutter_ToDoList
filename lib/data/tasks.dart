17490cc33ab09db6e83e081fd89c13a0210f444c
sebastienklaus <sebastienklaus7@gmail.com>
2022-02-01 23:42:19 +0100

    just change statut of task 3

--- lib/data/tasks.dart
+++ lib/data/tasks.dart
@@ -3,7 +3,7 @@ import 'package:todo_list/models/task.dart';
 List<Task> tasks = [
   Task(1, 'Groceries', false, DateTime.now()),
   Task(2, 'Homework', false, DateTime.now()),
-  Task(3, 'Buy flowers', false, DateTime.now()),
+  Task(3, 'Buy flowers', true, DateTime.now()),
   Task(4, 'Call parents', true, DateTime.now()),
   Task(5, 'Clean the bedroom', true, DateTime.now()),
 ];
