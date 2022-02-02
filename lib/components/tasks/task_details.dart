9361e2abc6e869bc3b8fa65f220f1b974f39dbd0
sebastienklaus <sebastienklaus7@gmail.com>
2022-02-02 16:26:34 +0100

    add task details

---
+++ lib/components/tasks/task_details.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/models/task.dart';
+
+class TaskDetails extends StatelessWidget {
+  const TaskDetails({Key? key, required this.task}) : super(key: key);
+  //constrcuteur
+  final Task task;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              const Text('Created at'),
+              Text('${task.createdAt}'),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              const Text('Task is done ?'),
+              Text('${task.completed}'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
