8b7c4ec11943ee43f454a3ead22304be860eaf1f
sebastienklaus <sebastienklaus7@gmail.com>
2022-02-02 10:50:58 +0100

    change ListTile to TaskPreview

--- lib/components/tasks/task_master.dart
+++ lib/components/tasks/task_master.dart
@@ -12,19 +12,7 @@ class TaskMaster extends StatelessWidget {
       child: ListView.builder(
         itemCount: dataTasks.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            leading: const Icon(Icons.check_box_outline_blank_rounded),
-            title: Text(dataTasks[index].content),
-            subtitle: Text('Created at ${dataTasks[index].createdAt}'),
-            trailing: const Icon(Icons.drag_handle_rounded),
-            onTap: () {
-              showModalBottomSheet<void>(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return TaskPreview(task: dataTasks[index]);
-                  });
-            },
-          );
+          return TaskPreview(task: dataTasks[index]);
         },
       ),
     );
