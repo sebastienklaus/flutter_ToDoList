ed5ddf9107d74cf56c333941e23c70c58435d6d5
sebastienklaus <sebastienklaus7@gmail.com>
2022-02-03 09:50:03 +0100

    add some tasks

--- lib/data/tasks.dart
+++ lib/data/tasks.dart
@@ -6,4 +6,9 @@ List<Task> tasks = [
   Task(3, 'Buy flowers', true, DateTime.now()),
   Task(4, 'Call parents', true, DateTime.now()),
   Task(5, 'Clean the bedroom', true, DateTime.now()),
+  Task(6, 'Clean the bedroom', true, DateTime.now()),
+  Task(7, 'Clean the bedroom', true, DateTime.now()),
+  Task(8, 'Clean the bedroom', true, DateTime.now()),
+  Task(9, 'Clean the bedroom', true, DateTime.now()),
+  Task(10, 'Clean the bedroom', true, DateTime.now()),
 ];
