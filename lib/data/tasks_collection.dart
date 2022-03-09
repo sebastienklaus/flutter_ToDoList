77bbc641cbc5e9aa9084ea1596ecd37ad41d5272
sebastienklaus <sebastienklaus7@gmail.com>
2022-03-09 15:22:34 +0100

    create class TasksCollection + 3 methods (cud)

---
+++ lib/data/tasks_collection.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+import 'package:todo_list/data/tasks.dart';
+
+class TasksCollection extends ChangeNotifier {
+  void create() {}
+
+  void update() {}
+
+  void delete() {}
+}
