9c9143d338d4a4cda8015c43464090de8804cf98
sebastienklaus <sebastienklaus7@gmail.com>
2022-03-10 12:09:33 +0100

    add form input + consumer (maybe it's wrong)

--- lib/components/tasks/task_form.dart
+++ lib/components/tasks/task_form.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_list/data/tasks_collection.dart';
 import 'package:todo_list/models/task.dart';
 
 class TaskForm extends StatefulWidget {
@@ -12,72 +14,81 @@ class TaskForm extends StatefulWidget {
 
 class _TaskFormState extends State<TaskForm> {
   final _formKey = GlobalKey<FormState>();
-  TimeOfDay? time = const TimeOfDay(hour: 12, minute: 12);
+  late bool checkedValue;
+  // * controller(s) about the input
   TextEditingController taskNameController = TextEditingController();
-  TextEditingController timeCtl = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     taskNameController =
         TextEditingController(text: widget.taskToUpdate!.content);
+    checkedValue = widget.taskToUpdate!.completed;
   }
 
   @override
   Widget build(BuildContext context) {
-    taskNameController.text = widget.taskToUpdate!.content;
-    return Form(
-      key: _formKey,
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        TextFormField(
-          controller: taskNameController,
-          // initialValue: taskNameController.text,
-          // The validator receives the text that the user has entered.
-          decoration: const InputDecoration(labelText: 'Nom de la tâche'),
-          validator: (value) {
-            if (value == null || value.isEmpty) {
-              return 'Please enter some text';
-            }
-            return null;
-          },
-        ),
-        // filed for pick DateTime
-        // TextFormField(
-        //   controller: timeCtl,
-        //   onTap: () async {
-        //     TimeOfDay? newTime =
-        //         await showTimePicker(context: context, initialTime: time!);
-        //     if (newTime != null && newTime != time) {
-        //       timeCtl.text = newTime.format(context);
-        //       setState(() {
-        //         time = newTime;
-        //       });
-        //     }
-        //   },
-        //   validator: (value) {
-        //     if (value == null || value.isEmpty) {
-        //       return 'The value of date is wrong !';
-        //     }
-        //     return null;
-        //   },
-        //   decoration: const InputDecoration(labelText: 'Choose a datetime'),
-        // ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16.0),
-          child: ElevatedButton(
-            onPressed: () {
-              // Validate returns true if the form is valid, or false otherwise.
-              if (_formKey.currentState!.validate()) {
-                print(taskNameController.text);
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Tâche ajoutée !')),
-                );
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Form(
+        key: _formKey,
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          TextFormField(
+            controller: taskNameController,
+            // The validator receives the text that the user has entered.
+            decoration: const InputDecoration(labelText: 'Nom de la tâche'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
               }
+              return null;
             },
-            child: const Text('Submit'),
           ),
-        ),
-      ]),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Tâche terminée',
+                style: TextStyle(fontSize: 16),
+              ),
+              Checkbox(
+                value: checkedValue,
+                onChanged: (newValue) {
+                  setState(() {
+                    checkedValue = newValue!;
+                    print(checkedValue);
+                  });
+                },
+              )
+            ],
+          ),
+          Consumer<TasksCollection>(
+            builder: (context, tasksCollection, child) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Validate returns true if the form is valid, or false otherwise.
+                    if (_formKey.currentState!.validate()) {
+                      print(taskNameController.text);
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Tâche ajoutée !')),
+                      );
+                      tasksCollection.updateTask(Task(
+                          widget.taskToUpdate!.id,
+                          taskNameController.text,
+                          checkedValue,
+                          DateTime.now()));
+                      Navigator.popAndPushNamed(context, '/all_tasks');
+                    }
+                  },
+                  child: const Text('Submit'),
+                ),
+              );
+            },
+          )
+        ]),
+      ),
     );
   }
 }
