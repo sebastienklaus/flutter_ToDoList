8ed1d2195251e41382be8c333a07e786916af932
sebastienklaus <sebastienklaus7@gmail.com>
2022-03-12 21:34:31 +0100

    add consumer

--- lib/screens/one_task.dart
+++ lib/screens/one_task.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todo_list/components/tasks/task_form.dart';
+import 'package:todo_list/data/tasks_collection.dart';
 import 'package:todo_list/models/task.dart';
 
-class OneTask extends StatelessWidget {
+class OneTask extends StatefulWidget {
   const OneTask({Key? key}) : super(key: key);
 
+  @override
+  State<OneTask> createState() => _OneTaskState();
+}
+
+class _OneTaskState extends State<OneTask> {
   @override
   Widget build(BuildContext context) {
     final Task? task = ModalRoute.of(context)!.settings.arguments as Task?;
@@ -13,6 +20,16 @@ class OneTask extends StatelessWidget {
         appBar: AppBar(
           title: Text('Modifier la tâche "${task!.content}"'),
         ),
-        body: TaskForm(taskToUpdate: task));
+        body: Consumer<TasksCollection>(
+            builder: (context, tasksCollection, child) {
+          return TaskForm(
+            taskToUpdate: task,
+            onUpdate: (newTask) {
+              setState(() {
+                tasksCollection.updateTask(newTask);
+              });
+            },
+          );
+        }));
   }
 }
