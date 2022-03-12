02757d10f2791984131e24be7160f41c49954983
sebastienklaus <sebastienklaus7@gmail.com>
2022-03-12 21:38:17 +0100

    remove unuse package

--- lib/components/tasks/task_form.dart
+++ lib/components/tasks/task_form.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:todo_list/data/tasks_collection.dart';
 import 'package:todo_list/models/task.dart';
 
 import 'package:todo_list/tools/showSnackBar.dart';
