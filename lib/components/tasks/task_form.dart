f0eddc49cbc38cf1745c00477df6f6bbbb0c77fa
sebastienklaus <sebastienklaus7@gmail.com>
2022-03-13 00:31:41 +0100

    RESOLVE NULL CHECK OPERATOR

--- lib/components/tasks/task_form.dart
+++ lib/components/tasks/task_form.dart
@@ -4,14 +4,9 @@ import 'package:todo_list/models/task.dart';
 import 'package:todo_list/tools/showSnackBar.dart';
 
 class TaskForm extends StatefulWidget {
-  const TaskForm(
-      {Key? key,
-      required this.typeOfAction,
-      this.taskToUpdate,
-      required this.onChangeTask})
+  const TaskForm({Key? key, this.taskToUpdate, required this.onChangeTask})
       : super(key: key);
 
-  final int typeOfAction;
   final Task? taskToUpdate;
   final Function onChangeTask;
 
@@ -28,85 +23,80 @@ class _TaskFormState extends State<TaskForm> {
   @override
   void initState() {
     super.initState();
-    taskNameController =
-        TextEditingController(text: widget.taskToUpdate!.content);
-    checkedValue = widget.taskToUpdate!.completed;
+    if (widget.taskToUpdate != null) {
+      taskNameController =
+          TextEditingController(text: widget.taskToUpdate!.content);
+      checkedValue = widget.taskToUpdate!.completed;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     // if it's an action of Create, then
-    if (widget.typeOfAction == 1) {
-      return Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Form(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [],
-        )),
-      );
-    }
+
     // if else this is an action of Update
-    else {
-      return Padding(
+
+    return Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Form(
-          key: _formKey,
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Checkbox(
-                  value: checkedValue,
-                  onChanged: (newValue) {
-                    setState(() {
-                      checkedValue = newValue!;
-                    });
-                  },
-                  shape: const CircleBorder(side: BorderSide(width: 2)),
-                ),
-                Flexible(
-                  child: TextFormField(
-                    controller: taskNameController,
-                    // The validator receives the text that the user has entered.
-                    decoration: const InputDecoration(
-                      border: InputBorder.none,
-                      // labelText: 'Nom de la tâche',
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Validate returns true if the form is valid, or false otherwise.
-                  if (_formKey.currentState!.validate()) {
-                    widget.onChangeTask(Task(widget.taskToUpdate!.id,
-                        taskNameController.text, checkedValue, DateTime.now()));
-                    Navigator.pop(context);
-                    ScaffoldMessenger.of(context).showSnackBar(snackBarMessage()
-                        .info('Une tâche vient d\'être modifiée'));
-                  }
-                },
-                child: const Text('Submit'),
-              ),
-            )
-          ]),
-        ),
-      );
-    }
+        child: widget.taskToUpdate != null
+            ? Form(
+                key: _formKey,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Checkbox(
+                            value: checkedValue,
+                            onChanged: (newValue) {
+                              setState(() {
+                                checkedValue = newValue!;
+                              });
+                            },
+                            shape:
+                                const CircleBorder(side: BorderSide(width: 2)),
+                          ),
+                          Flexible(
+                            child: TextFormField(
+                              controller: taskNameController,
+                              // The validator receives the text that the user has entered.
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                // labelText: 'Nom de la tâche',
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter some text';
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            // Validate returns true if the form is valid, or false otherwise.
+                            if (_formKey.currentState!.validate()) {
+                              widget.onChangeTask(Task(
+                                  widget.taskToUpdate!.id,
+                                  taskNameController.text,
+                                  checkedValue,
+                                  DateTime.now()));
+                              Navigator.pop(context);
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  snackBarMessage().info(
+                                      'Une tâche vient d\'être modifiée'));
+                            }
+                          },
+                          child: const Text('Submit'),
+                        ),
+                      )
+                    ]),
+              )
+            : const Text('s'));
   }
 }
-// const SnackBar(
-//                         backgroundColor: Color(0xff17a2b8),
-//                         content: Text('Tâche modifiée !')),
-//                   );
