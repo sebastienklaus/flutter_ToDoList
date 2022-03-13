85c332461835da07a25e5d4842766453a1fa504e
sebastienklaus <sebastienklaus7@gmail.com>
2022-03-13 10:42:36 +0100

    add description

--- lib/data/tasks.dart
+++ lib/data/tasks.dart
@@ -1,15 +1,22 @@
 import 'package:todo_list/models/task.dart';
 
 List<Task> tasks = [
-  Task(1, 'Groceries', true, DateTime.now()),
-  Task(2, 'Homework', true, DateTime.now()),
-  Task(3, 'Buy flowers', true, DateTime.now()),
-  Task(4, 'Call parents', true, DateTime.now()),
-  Task(5, 'Clean the bedroom', false, DateTime.now()),
-  Task(6, 'Learn Flutter & Dart', true, DateTime.now()),
-  Task(7, 'Learn Slim framework', false, DateTime.now()),
-  Task(8, 'Read Flutter Docs about ListView.builder()', false, DateTime.now()),
-  Task(9, 'Do some VueJS', false, DateTime.now()),
-  Task(10, 'Think about Geofencing project', true, DateTime.now()),
-  Task(10, 'Think about Geofencing project', true, DateTime.now())
+  Task(1, 'Groceries', 'Lorem ipsum dolor sit amet.', true, DateTime.now()),
+  Task(2, 'Homework', 'Lorem ipsum dolor sit amet.', true, DateTime.now()),
+  Task(3, 'Buy flowers', 'Lorem ipsum dolor sit amet.', true, DateTime.now()),
+  Task(4, 'Call parents', 'Lorem ipsum dolor sit amet.', true, DateTime.now()),
+  Task(5, 'Clean the bedroom', 'Lorem ipsum dolor sit amet.', false,
+      DateTime.now()),
+  Task(6, 'Learn Flutter & Dart', 'Lorem ipsum dolor sit amet.', true,
+      DateTime.now()),
+  Task(7, 'Learn Slim framework', 'Lorem ipsum dolor sit amet.', false,
+      DateTime.now()),
+  Task(8, 'Read Flutter Docs about ListView.builder()',
+      'Lorem ipsum dolor sit amet.', false, DateTime.now()),
+  Task(
+      9, 'Do some VueJS', 'Lorem ipsum dolor sit amet.', false, DateTime.now()),
+  Task(10, 'Think about Geofencing project', 'Lorem ipsum dolor sit amet.',
+      true, DateTime.now()),
+  Task(10, 'Think about Geofencing project', 'Lorem ipsum dolor sit amet.',
+      true, DateTime.now())
 ];
--- lib/models/task.dart
+++ lib/models/task.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 class Task extends StatelessWidget {
   int id; //id of task
   String content; //content of the task
+  String description;
   bool completed; //bool if the task is complete or not
   DateTime createdAt; //time of creation of task
 
-  Task(this.id, this.content, this.completed, this.createdAt, {Key? key})
+  Task(this.id, this.content, this.description, this.completed, this.createdAt,
+      {Key? key})
       : super(key: key);
 
   @override
