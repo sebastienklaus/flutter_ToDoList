d43ee3e88aeac8db5839ad07334dd088a2d84bef
sebastienklaus <sebastienklaus7@gmail.com>
2022-03-14 12:08:12 +0100

    add danger method

--- lib/tools/showSnackBar.dart
+++ lib/tools/showSnackBar.dart
@@ -59,4 +59,25 @@ class snackBarMessage {
       ),
     );
   }
+
+  //method about a SnackBar for info action
+  SnackBar danger(String message) {
+    return SnackBar(
+      backgroundColor: const Color(0xffdc3545),
+      duration: const Duration(seconds: 2),
+      behavior: SnackBarBehavior.floating,
+      content: Row(
+        children: [
+          const Icon(
+            Icons.dangerous_outlined,
+            color: Colors.white,
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          Expanded(child: Text(message))
+        ],
+      ),
+    );
+  }
 }
