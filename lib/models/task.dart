2ef8927f26037215bcc077945d75e7938ccebc6a
sebastienklaus <sebastienklaus7@gmail.com>
2022-03-16 17:18:27 +0100

    remoove stless widget

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class Task extends StatelessWidget {
+class Task {
   int id; //id of task
   String content; //content of the task
   // String description;
   bool completed; //bool if the task is complete or not
   DateTime createdAt; //time of creation of task
 
-  Task(this.id, this.content, this.completed, this.createdAt, {Key? key})
-      : super(key: key);
+  Task(this.id, this.content, this.completed, this.createdAt, {Key? key});
   // Task(this.id, this.content, this.description, this.completed, this.createdAt,
-  //     {Key? key})
-  //     : super(key: key);
+  //     {Key? key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
 }
