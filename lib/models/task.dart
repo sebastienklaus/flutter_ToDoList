343d119da87f0c52a5fd283690bc8df8019d9f2e
sebastienklaus <sebastienklaus7@gmail.com>
2022-03-16 17:49:28 +0100

    change task constructor

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
+import 'package:dio/dio.dart';
 
 class Task {
   int id; //id of task
@@ -8,8 +9,9 @@ class Task {
   bool completed; //bool if the task is complete or not
   DateTime createdAt; //time of creation of task
 
-  Task(this.id, this.content, this.completed, this.createdAt, {Key? key});
+  Task(this.id, this.content, this.completed, this.createdAt);
   // Task(this.id, this.content, this.description, this.completed, this.createdAt,
   //     {Key? key});
 
+  // Task.fromJson();
 }
