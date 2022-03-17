65758c51f6903758d62b86719149f1c407e4237d
sebastienklaus <sebastienklaus7@gmail.com>
2022-03-17 14:06:55 +0100

    remove comments

--- lib/data/tasks.dart
+++ lib/data/tasks.dart
@@ -13,23 +13,3 @@ List<Task> tasks = [
   Task(10, 'Think about Geofencing project', true, DateTime.now()),
   Task(10, 'Think about Geofencing project', true, DateTime.now())
 ];
-// List<Task> tasks = [
-//   Task(1, 'Groceries', 'Lorem ipsum dolor sit amet.', true, DateTime.now()),
-//   Task(2, 'Homework', 'Lorem ipsum dolor sit amet.', true, DateTime.now()),
-//   Task(3, 'Buy flowers', 'Lorem ipsum dolor sit amet.', true, DateTime.now()),
-//   Task(4, 'Call parents', 'Lorem ipsum dolor sit amet.', true, DateTime.now()),
-//   Task(5, 'Clean the bedroom', 'Lorem ipsum dolor sit amet.', false,
-//       DateTime.now()),
-//   Task(6, 'Learn Flutter & Dart', 'Lorem ipsum dolor sit amet.', true,
-//       DateTime.now()),
-//   Task(7, 'Learn Slim framework', 'Lorem ipsum dolor sit amet.', false,
-//       DateTime.now()),
-//   Task(8, 'Read Flutter Docs about ListView.builder()',
-//       'Lorem ipsum dolor sit amet.', false, DateTime.now()),
-//   Task(
-//       9, 'Do some VueJS', 'Lorem ipsum dolor sit amet.', false, DateTime.now()),
-//   Task(10, 'Think about Geofencing project', 'Lorem ipsum dolor sit amet.',
-//       true, DateTime.now()),
-//   Task(10, 'Think about Geofencing project', 'Lorem ipsum dolor sit amet.',
-//       true, DateTime.now())
-// ];
